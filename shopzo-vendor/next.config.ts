5297553194f04bfe7e0889c47a988b55575aa6d1
EC2 Default User <ec2-user@ip-172-31-18-185.ec2.internal>
2026-04-18 11:42:37 +0000

    done with nginx setup , ssl

--- shopzo-vendor/next.config.ts
+++ shopzo-vendor/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/vendor',
+  assetPrefix: '/vendor',
   reactCompiler: true,
 };
 
